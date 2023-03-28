9f7505be7cb5925db0bd8ec5288cd60e8cbc0bf5
sato <ksato100430066@gmail.com>
2023-03-28 20:01:17 +0900

    added wid

--- Dataset/base.py
+++ Dataset/base.py
@@ -11,7 +11,7 @@ import torch
 module_path = os.path.abspath("../fprocess")
 sys.path.append(module_path)
 
-import fprocess
+from fprocess import fprocess
 
 
 class Dataset:
@@ -187,60 +187,64 @@ class Dataset:
                 ndx = self._output_indices(__index)
                 tgt_indices.append(ndx.start)
             indices = tgt_indices
+        # print(f"start revert procress for {[__process.kinds for __process in self.processes]}")
         for p_index in range(len(self.processes)):
             r_index = len(self.processes) - 1 - p_index
             process = self.processes[r_index]
-            params = process.revert_params
-            if len(params) == 1:
-                r_data = process.revert(r_data)
-            else:
-                params = {}
-                if process.kinds == fprocess.MinMaxPreProcess.kinds:
+            if hasattr(process, "revert_params"):
+                # print(f"currently: {r_data[0, 0]}")
+                params = process.revert_params
+                if len(params) == 1:
                     r_data = process.revert(r_data)
-                elif process.kinds == fprocess.SimpleColumnDiffPreProcess.kinds:
-                    close_column = process.base_column
-                    if p_index > 0:
-                        processes = self.processes[:p_index]
-                        required_length = [1]
-                        base_processes = []
-                        for base_process in processes:
-                            if close_column in base_process.columns:
-                                base_processes.append(base_process)
-                                required_length.append(base_process.get_minimum_required_length())
-                        if len(base_processes) > 0:
-                            raise Exception("Not implemented yet")
-                    base_indices = [index - 1 for index in indices]
-                    base_values = self.org_data[close_column].iloc[base_indices]
-                    r_data = process.revert(r_data, base_value=base_values)
-                elif process.kinds == fprocess.DiffPreProcess.kinds:
-                    target_columns = process.columns
-                    if p_index > 0:
-                        processes = self.processes[:p_index]
-                        required_length = [process.get_minimum_required_length()]
-                        base_processes = []
-                        for base_process in processes:
-                            if len(set(target_columns) & set(base_process.columns)) > 0:
-                                base_processes.append(base_process)
-                                required_length.append(base_process.get_minimum_required_length())
-                        if len(base_processes) > 0:
-                            required_length = max(required_length)
-                            batch_base_indices = [index - required_length for index in indices]
-                            batch_base_values = pd.DataFrame()
-                            for index in batch_base_indices:
-                                target_data = self.org_data[target_columns].iloc[index : index + required_length]
-                                for base_process in base_processes:
-                                    target_data = base_process(target_data)
-                                batch_base_values = pd.concat([batch_base_values, target_data.iloc[-1:]], axis=0)
-                            batch_base_values = batch_base_values.values.reshape(1, *batch_base_values.shape)
+                else:
+                    params = {}
+                    if process.kinds == fprocess.MinMaxPreProcess.kinds:
+                        r_data = process.revert(r_data)
+                    elif process.kinds == fprocess.SimpleColumnDiffPreProcess.kinds:
+                        close_column = process.base_column
+                        if p_index > 0:
+                            processes = self.processes[:p_index]
+                            required_length = [1]
+                            base_processes = []
+                            for base_process in processes:
+                                if close_column in base_process.columns:
+                                    base_processes.append(base_process)
+                                    required_length.append(base_process.get_minimum_required_length())
+                            if len(base_processes) > 0:
+                                raise Exception("Not implemented yet")
+                        base_indices = [index - 1 for index in indices]
+                        base_values = self.org_data[close_column].iloc[base_indices]
+                        r_data = process.revert(r_data, base_value=base_values)
+                    elif process.kinds == fprocess.DiffPreProcess.kinds:
+                        target_columns = process.columns
+                        if r_index > 0:
+                            processes = self.processes[:r_index]
+                            required_length = [process.get_minimum_required_length()]
+                            base_processes = []
+                            for base_process in processes:
+                                if len(set(target_columns) & set(base_process.columns)) > 0:
+                                    base_processes.append(base_process)
+                                    required_length.append(base_process.get_minimum_required_length())
+                            if len(base_processes) > 0:
+                                required_length = max(required_length)
+                                batch_base_indices = [index - required_length for index in indices]
+                                batch_base_values = pd.DataFrame()
+                                print(f"  apply {[__process.kinds for __process in base_processes]} to revert diff")
+                                for index in batch_base_indices:
+                                    target_data = self.org_data[target_columns].iloc[index : index + required_length]
+                                    for base_process in base_processes:
+                                        target_data = base_process(target_data)
+                                    batch_base_values = pd.concat([batch_base_values, target_data.iloc[-1:]], axis=0)
+                                batch_base_values = batch_base_values.values.reshape(1, *batch_base_values.shape)
+                            else:
+                                base_indices = [index - 1 for index in indices]
+                                batch_base_values = self.org_data[target_columns].iloc[base_indices]
                         else:
                             base_indices = [index - 1 for index in indices]
                             batch_base_values = self.org_data[target_columns].iloc[base_indices]
+                        r_data = process.revert(r_data, base_values=batch_base_values)
                     else:
-                        base_indices = [index - 1 for index in indices]
-                        batch_base_values = self.org_data[target_columns].iloc[base_indices]
-                    r_data = process.revert(r_data, base_values=batch_base_values)
-                else:
-                    raise Exception(f"Not implemented: {process.kinds}")
+                        raise Exception(f"Not implemented: {process.kinds}")
         return r_data
 
 
